6c0fd4d79678d50965b3e0c6b4d5db0da9890de3
Joseph vantassel <jvantassel@utexas.edu>
2019-11-05 19:19:22 -0600

    :sparkles: Add recall to file location

--- gm_gui.py
+++ gm_gui.py
@@ -9,7 +9,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import time
+import json
+import os
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -22,14 +23,14 @@ class GroundMotionProcessing():
         self.master = master
         self.init_layout()
 
-        self.fig1, self.ax1 = plt.subplots(nrows=1, ncols=1, figsize=(5, 3))
+        self.fig1, self.ax1 = plt.subplots(nrows=1, ncols=1, figsize=(5, 2.5))
         self.canvas1 = FigureCanvasTkAgg(self.fig1, self.frame2)
         self.canvas1.get_tk_widget().grid(row=1, column=0)
         tb1 = NavigationToolbar2Tk(self.canvas1, self.frame2_1)
         tb1.update()
         self.canvas1._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
 
-        self.fig2, self.ax2 = plt.subplots(nrows=1, ncols=1, figsize=(5, 3))
+        self.fig2, self.ax2 = plt.subplots(nrows=1, ncols=1, figsize=(5, 2.5))
         self.canvas2 = FigureCanvasTkAgg(self.fig2, self.frame4)
         self.canvas2.get_tk_widget().grid(row=1, column=1)
         tb2 = NavigationToolbar2Tk(self.canvas2, self.frame4_1)
@@ -46,7 +47,7 @@ class GroundMotionProcessing():
         self.ax1.set_xlabel("Time (s)")
         self.ax1.set_ylabel("Amplitude")
         self.canvas1.draw()
- 
+
     def update_fseries_plot(self):
         logging.info("begin - update_fseries_plot()")
         if bool(self.fseries):
@@ -56,16 +57,11 @@ class GroundMotionProcessing():
         self.ax2.set_xlabel("Frequency (Hz)")
         self.ax2.set_ylabel("Amplitude")
         self.ax2.set_xscale("log")
-        # xlim = self.ax2.get_xlim()
-        # self.ax2.set_xticks([0.01, 0.03, 0.1, 0.3, 1, 3, 10, 100, 300, 1000])
-        # self.ax2.set_xlim(xlim)
         self.canvas2.draw()
 
     def init_layout(self):
         logging.info("begin - init_layout()")
         title_font = 'Arial 11 underline'
-        subtitle_font = 'Arial 10'
-        # std_font = 'Arial 10 underline'
 
         frame_bg = '#ffffff'
         label_bg = '#ffffff'
@@ -74,13 +70,14 @@ class GroundMotionProcessing():
         button_bg = frame_bg
         check_bg = frame_bg
 
-        self.master.minsize(6, 8)
+        self.master.minsize(6, 6)
+
 
         # Frame 1 ----------------------------------------------------
         frame1 = Frame(bg=frame_bg)
         frame1.grid(row=0, column=0)
 
-        title_1 = Label(frame1, text="Time Domain Settings",
+        title_1 = Label(frame1, text="Time Domain",
                         bg=label_bg, font=title_font)
         title_1.grid(row=0, columnspan=2)
 
@@ -93,6 +90,11 @@ class GroundMotionProcessing():
                             bg=label_bg)
         label_1_1_4 = Label(frame1, text="Filter Order (#):", bg=label_bg)
 
+        label_1_1_1.grid(row=2, column=0, sticky=E)
+        label_1_1_2.grid(row=3, column=0, sticky=E)
+        label_1_1_3.grid(row=4, column=0, sticky=E)
+        label_1_1_4.grid(row=5, column=0, sticky=E)
+
         self.apply_filter = BooleanVar()
         self.bp_flow = StringVar()
         self.bp_fhigh = StringVar()
@@ -104,11 +106,6 @@ class GroundMotionProcessing():
         input_1_1_3 = Entry(frame1, bg=entry_bg, textvariable=self.bp_fhigh)
         input_1_1_4 = Entry(frame1, bg=entry_bg, textvariable=self.bp_fn)
 
-        label_1_1_1.grid(row=2, column=0, sticky=E)
-        label_1_1_2.grid(row=3, column=0, sticky=E)
-        label_1_1_3.grid(row=4, column=0, sticky=E)
-        label_1_1_4.grid(row=5, column=0, sticky=E)
-
         input_1_1_1.grid(row=2, column=1)
         input_1_1_2.grid(row=3, column=1)
         input_1_1_3.grid(row=4, column=1)
@@ -118,22 +115,20 @@ class GroundMotionProcessing():
         self.cos_width = StringVar()
         self.apply_taper = BooleanVar()
         label_1_2_1 = Label(frame1, text="Apply Cosine Taper:", bg=label_bg)
+        label_1_2_2 = Label(frame1, text="Width (%):", bg=label_bg)
+
+        label_1_2_1.grid(row=7, column=0, sticky=E)
+        label_1_2_2.grid(row=8, column=0, sticky=E)
+
         input_1_2_1 = Checkbutton(frame1, bg=check_bg,
                                   variable=self.apply_taper,
                                   disabledforeground=check_bg)
-        label_1_2_2 = Label(frame1, text="Width (%):", bg=label_bg)
         input_1_2_2 = Entry(frame1, bg=entry_bg, textvariable=self.cos_width)
 
-        label_1_2_1.grid(row=7, column=0, sticky=E)
         input_1_2_1.grid(row=7, column=1)
-        label_1_2_2.grid(row=8, column=0, sticky=E)
         input_1_2_2.grid(row=8, column=1)
 
         # Run / Undo / New File
-        # subtitle_1_3 = Label(frame1, text="Apply Changes",
-        #                      bg=label_bg, font=subtitle_font)
-        # subtitle_1_3.grid(row=9, columnspan=2)
-
         subframe = Frame(frame1)
         subframe.grid(row=9, columnspan=2)
         input_1_3_1 = Button(subframe, command=self.edit_tseries,
@@ -150,85 +145,73 @@ class GroundMotionProcessing():
         input_1_3_3.grid(row=0, column=3)
         input_1_3_4.grid(row=0, column=4)
 
+
         # Frame 2 ----------------------------------------------------
         self.frame2 = Frame(bg=frame_bg)
         self.frame2.grid(row=0, column=1)
         self.frame2_1 = Frame(self.master, bg=frame_bg)
         self.frame2_1.grid(row=1, column=1)
 
+
         # Frame 3 ----------------------------------------------------
         frame3 = Frame(bg=frame_bg, bd=1)
         frame3.grid(row=2, column=0, ipadx=1, ipady=1)
 
-        title_3 = Label(frame3, text="Frequency Domain Settings",
+        title_3 = Label(frame3, text="Frequency Domain",
                         bg=label_bg, font=title_font)
         title_3.grid(row=0, columnspan=2)
 
         # Smoothing
-        subtitle_3_1 = Label(frame3, text="Konno and Ohmachi Smoothing",
-                             bg=label_bg, font=subtitle_font)
-        subtitle_3_1.grid(row=1, columnspan=2)
+        label_3_1_1 = Label(frame3, text="Apply Cosine Taper:", bg=label_bg)
+        label_3_1_2 = Label(frame3, text="Smoothing Constant (#):", bg=label_bg)
 
-        label_3_1_2 = Label(frame3, text="Bandwidth, b (#):", bg=label_bg)
+        label_3_1_1.grid(row=2, column=0, sticky=E)        
+        label_3_1_2.grid(row=3, column=0, sticky=E)
 
         self.apply_smoothing = BooleanVar()
         self.smooth_width = StringVar()
-        input_3_1_1 = Checkbutton(frame3, text="Apply Smoothing",
+        input_3_1_1 = Checkbutton(frame3, bg=check_bg,
                                   variable=self.apply_smoothing,
-                                  bg=check_bg, disabledforeground=check_bg)
+                                  disabledforeground=check_bg)
         input_3_1_2 = Entry(frame3, bg=entry_bg,
                             textvariable=self.smooth_width)
 
-        label_3_1_2.grid(row=3, column=0, sticky=E)
-
-        input_3_1_1.grid(row=2, columnspan=2)
+        input_3_1_1.grid(row=2, column=1)
         input_3_1_2.grid(row=3, column=1)
 
         # Resampling
-        subtitle_3_2 = Label(frame3, text="Resample",
-                             bg=label_bg, font=subtitle_font)
-        subtitle_3_2.grid(row=4, columnspan=2)
-
+        label_3_2_1 = Label(frame3, text="Apply Resampling:", bg=label_bg)
         label_3_2_2 = Label(frame3, text="Lower Frequency Limit (Hz):",
                             bg=label_bg)
         label_3_2_3 = Label(frame3, text="Upper Frequency Limit (Hz):",
                             bg=label_bg)
         label_3_2_4 = Label(frame3, text="Number of Samples (#)", bg=label_bg)
 
+        label_3_2_1.grid(row=5, column=0, sticky=E)
+        label_3_2_2.grid(row=6, column=0, sticky=E)
+        label_3_2_3.grid(row=7, column=0, sticky=E)
+        label_3_2_4.grid(row=8, column=0, sticky=E)
+
         self.apply_resampling = BooleanVar()
         self.res_flow = StringVar()
         self.res_fhigh = StringVar()
         self.res_fn = StringVar()
-        input_3_2_1 = Checkbutton(frame3, text="Resample",
-                                  variable=self.apply_resampling, 
+        input_3_2_1 = Checkbutton(frame3, variable=self.apply_resampling,
                                   bg=check_bg, disabledforeground=check_bg)
         input_3_2_2 = Entry(frame3, bg=entry_bg, textvariable=self.res_flow)
         input_3_2_3 = Entry(frame3, bg=entry_bg, textvariable=self.res_fhigh)
         input_3_2_4 = Entry(frame3, bg=entry_bg, textvariable=self.res_fn)
 
-        label_3_2_2.grid(row=6, column=0, sticky=E)
-        label_3_2_3.grid(row=7, column=0, sticky=E)
-        label_3_2_4.grid(row=8, column=0, sticky=E)
-
-        input_3_2_1.grid(row=5, columnspan=2)
+        input_3_2_1.grid(row=5, column=1)
         input_3_2_2.grid(row=6, column=1)
         input_3_2_3.grid(row=7, column=1)
         input_3_2_4.grid(row=8, column=1)
 
         # Run / Undo / New File
-        # subtitle_3_3 = Label(frame3, text="Apply Changes",
-        #                      bg=label_bg, font=subtitle_font)
-        # subtitle_3_3.grid(row=9, columnspan=2)
-
         input_3_3_1 = Button(frame3, command=self.edit_fseries,
                              text="Apply", bg=button_bg)
-        # input_3_3_2 = Button(frame1, command=self.reload_file,
-        #                      text="Undo", bg=button_bg)
-
         input_3_3_1.grid(row=10, columnspan=2)
-        # input_3_3_2.grid(row=11, columnspan=2)
-        # input_3_3_3.grid(row=12, columnspan=2)
-        # input_3_3_4.grid(row=13, columnspan=2)
+
 
         # Frame 4 ----------------------------------------------------
         self.frame4 = Frame(bg=frame_bg)
@@ -260,7 +243,8 @@ class GroundMotionProcessing():
         # Apply Cosine Taper
         if self.apply_taper.get() and not self.applied_taper:
             try:
-                self.tseries.cosine_taper(width=float(self.cos_width.get())/100)
+                self.tseries.cosine_taper(
+                    width=float(self.cos_width.get())/100)
                 self.applied_taper = True
             except:
                 pass
@@ -275,7 +259,8 @@ class GroundMotionProcessing():
         if self.apply_smoothing.get() and not self.applied_smoothing:
             logging.info("  applying_smooting")
             try:
-                self.fseries.smooth_konno_ohmachi(float(self.smooth_width.get()))
+                self.fseries.smooth_konno_ohmachi(
+                    float(self.smooth_width.get()))
                 self.applied_smoothing = True
             except:
                 pass
@@ -285,10 +270,10 @@ class GroundMotionProcessing():
             logging.info("  applying_resampling")
             try:
                 self.fseries.resample(float(self.res_flow.get()),
-                                    float(self.res_fhigh.get()),
-                                    int(self.res_fn.get()),
-                                    res_type='log',
-                                    inplace=True)
+                                      float(self.res_fhigh.get()),
+                                      int(self.res_fn.get()),
+                                      res_type='log',
+                                      inplace=True)
                 self.applied_resampling = True
             except:
                 pass
@@ -298,9 +283,22 @@ class GroundMotionProcessing():
 
     def load_new_file(self):
         logging.info("begin - load_new_file()")
-        resp = filedialog.askopenfilename(
-            initialdir=r"D:\CurrentResearch\signalprocessing\gm_gui\data")
+        # initialdir = r"D:\CurrentResearch\signalprocessing\gm_gui\data"
+        settings_fname = "gm_gui_settings.json"
+        if os.path.exists(settings_fname):
+            with open(settings_fname, "r") as f:
+                settings = json.load(f)
+            initialdir = settings["initialdir"]
+        else:
+            initialdir = r"C:\\"
+        resp = filedialog.askopenfilename(initialdir=initialdir)
         # resp = r"D:\CurrentResearch\signalprocessing\gm_gui\data\UT.STN11.A2_C50.miniseed"
+
+        settings = {}
+        settings["initialdir"] = os.path.dirname(resp)
+        with open(settings_fname, "w") as f:
+            json.dump(settings, f)
+            
         self.fname = False
 
         logging.debug(f"    resp = {resp}")
@@ -334,6 +332,7 @@ class GroundMotionProcessing():
         logging.info("begin - on_closing()")
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             self.master.quit()
+
 
 root = Tk()
 root.title("Ground Motion Processing")
